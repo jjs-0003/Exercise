e2561b25f4cc19a901ea6f934a37d6c1da1cce21
tmy <jjs-0003@outlook.jp>
2014-05-12 16:14:26 +0900

    String commit

---
+++ src/jp/co/jjs/java_seminar/exercise_20140512_2/Exercise2.java
@@ -0,0 +1,14 @@
+package jp.co.jjs.java_seminar.exercise_20140512_2;
+
+public class Exercise2 {
+    public static void main(String[] args) {
+        String str1 = "今日の天気は";
+        String str2 = "今日の気温は";
+        String wether = "曇り";
+        int    kion   = 24;
+        
+        System.out.println(str1+wether+"です");
+        System.out.println(str2+kion+"です。");
+        
+    }
+}
