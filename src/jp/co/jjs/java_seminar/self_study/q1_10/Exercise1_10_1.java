0c55ade47f06bf6f0e1f4658146443a27122b72f
tmy <jjs-0003@outlook.jp>
2014-05-12 16:56:37 +0900

    1_10_1修正

--- src/jp/co/jjs/java_seminar/self_study/q1_10/Exercise1_10_1.java
+++ src/jp/co/jjs/java_seminar/self_study/q1_10/Exercise1_10_1.java
@@ -2,6 +2,6 @@ package jp.co.jjs.java_seminar.self_study.q1_10;
 
 public class Exercise1_10_1 {
     public static void main(String[] args) {
-        System.out.println("My name is Taro Sapporo.");
+        System.out.println("\"My name is Taro Sapporo.\"");
     }
 }
