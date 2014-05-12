2abaf28fc44a56d80818dd51a46d12938ac5b3d4
tmy <jjs-0003@outlook.jp>
2014-05-12 15:37:53 +0900

    Exercise1 commit

---
+++ src/jp/co/jjs/java_seminar/exercise_20140512_1/Exercise1.java
@@ -0,0 +1,25 @@
+package jp.co.jjs.java_seminar.exercise_20140512_1;
+
+public class Exercise1 {
+
+    public static void main(String[] args) {
+        int     a = 10;
+        float   b = 10.0f;
+        double  c = 10.111111;
+        char    d = 'a';
+        long    e = 10L;
+        boolean f = true;
+        short   g = 1;
+        byte    h = 8;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+
+    }
+}
