850d6750278dde68285271e0fcaf9c486a7901fc
tmy <jjs-0003@outlook.jp>
2014-05-13 10:54:33 +0900

    Exercise4 commit

---
+++ src/jp/co/jjs/java_seminar/exercise_20140513_02/Exercise4.java
@@ -0,0 +1,15 @@
+package jp.co.jjs.java_seminar.exercise_20140513_02;
+
+public class Exercise4 {
+   public static void main(String[] args) {
+       short a = 1;
+       short b = 5;
+       int   c;
+
+       c = a + b;
+
+       System.out.println(a);
+       System.out.println(b);
+       System.out.println(c);
+   }
+}
