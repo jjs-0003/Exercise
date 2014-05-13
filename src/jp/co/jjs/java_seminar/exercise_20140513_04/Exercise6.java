5e7936d15d9340fcea1f307b054e9b96c1dd154e
tmy <jjs-0003@outlook.jp>
2014-05-13 11:58:25 +0900

    Exercise6 commit

---
+++ src/jp/co/jjs/java_seminar/exercise_20140513_04/Exercise6.java
@@ -0,0 +1,14 @@
+package jp.co.jjs.java_seminar.exercise_20140513_04;
+
+public class Exercise6 {
+    public static void main(String[] args) {
+        int n = 6;
+
+        if(n % 2 == 0){
+            System.out.println("nは偶数です。");
+        }
+        else{
+            System.out.println("nは奇数です。");
+        }
+    }
+}
