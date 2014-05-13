8d7f22dafd8f3a2e15fe877fea80c0b23b109906
tmy <jjs-0003@outlook.jp>
2014-05-13 16:46:12 +0900

    self_study question1_14 commit

---
+++ src/jp/co/jjs/java_seminar/self_study/q1_14/Exercise1.java
@@ -0,0 +1,9 @@
+package jp.co.jjs.java_seminar.self_study.q1_14;
+
+public class Exercise1 {
+    public static void main(String[] args) {
+        /**/                                              //1.正しい
+        /*System.out.println("this is a test"); */        //2.正しい
+
+    }
+}
