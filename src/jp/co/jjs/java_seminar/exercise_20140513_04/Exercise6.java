95172f991639cb35b48bc01d9acece44ca10c25e
tmy <jjs-0003@outlook.jp>
2014-05-13 13:18:49 +0900

    Exercise7 commit

--- src/jp/co/jjs/java_seminar/exercise_20140513_04/Exercise6.java
+++ src/jp/co/jjs/java_seminar/exercise_20140513_04/Exercise6.java
@@ -2,7 +2,7 @@ package jp.co.jjs.java_seminar.exercise_20140513_04;
 
 public class Exercise6 {
     public static void main(String[] args) {
-        int n = 6;
+        int n = 9;
 
         if(n % 2 == 0){
             System.out.println("nは偶数です。");
