094fe257f2e9d273d8cb1cb7ace07f1b7e0dd6f8
tmy <jjs-0003@outlook.jp>
2014-05-13 11:16:40 +0900

    Exercise5 commit

---
+++ src/jp/co/jjs/java_seminar/exercise_20140513_03/Exercise5.java
@@ -0,0 +1,18 @@
+package jp.co.jjs.java_seminar.exercise_20140513_03;
+
+public class Exercise5 {
+    public static void main(String[] args) {
+        byte a;
+        int  b;
+        
+        b = 127;
+        a = (byte) b;
+        
+        System.out.println(a);
+        
+        b = 258;
+        a = (byte) b;
+        
+        System.out.println(a);
+    }
+}
